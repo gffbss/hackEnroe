f3f7b1bf5b299dfaba80910fe9838758daa0d0fc
Kevin Holloway <hollowaykj@gmail.com>
2014-02-22 12:28:33 -0800

    Adding JS

--- js/main.js
+++ js/main.js
@@ -7,28 +7,40 @@ function populateUsers() {
 	//The following is ASSUIMG classes and ids.
 
 	user1 = {
-		Name: "Geoff",
-		faceUrl: "img/geoff.jpg"
+		Geoff:"Test",
+		Username: "geoff5"
 	}
+
+	myDataRef.child(user1.Username).set(user1);
+
+	var test= user1.Username;
+
 	user2 = {
 		Name: "Kevin",
-		faceUrl: "img/kevin.jpg"
+		faceUrl: "img/kevin.jpeg",
+		Catchphrase: "The Legend"
 	}
+
 	user3 = {
 		Name: "Joeri",
-		faceUrl: "img/Joeri.img"
+		faceUrl: "img/joeri.jpg",
+		Catchphrase: "Swiss Army Knife"
 	}
 	user4 = {
 		Name: "John",
-		faceUrl: "img/John.img"
+		faceUrl: "img/johnny.jpeg",
+		Catchphrase: "Johnny Be Good"
 	}
 
+	myDataRef.on('value', function(snapshot) {
+  		console.log(snapshot.val().test)
+	});
 
 	users = [user1, user2, user3, user4]
 
 	for (i=0; i<users.length; i++)
 	{
-		$('#userList').append("<li><a href src='"+ users[i].faceUrl+ "'>Here's " + users[i].Name + "</a></li>");
+		$('#userList').append('<div class="col-xs-6 col-sm-3 placeholder"><img data-src="'+ users[i].faceUrl +'" class="img-responsive" alt="200x200" src="'+users[i].faceUrl+'"><h4>'+ users[i].Name+'</h4><span class="text-muted">'+users[i].Catchphrase+'</span></div>')
 	}
 
 }
@@ -38,7 +50,6 @@ function populateList() {
 	list1 = {
 		Name: "To Do",
 		listUrl: "todo.jpg"
-
 	}
 
 	list2 = {
